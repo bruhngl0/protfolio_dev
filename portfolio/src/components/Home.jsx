9653a0c6beb76fa79082904946db837c24f9d419
bruh.ngl0 <adityashrm500@gmail.com>
2023-08-16 05:09:56 +0530

    unbelievable

--- portfolio/src/components/Home.jsx
+++ portfolio/src/components/Home.jsx
@@ -6,8 +6,10 @@ import { SiExpress,  SiMongodb, SiGithub} from "react-icons/si"
 const Home = () => {
   return (
     <>
-    <main className='home-main' id= 'home'>
-    <section className = 'home-one'>
+
+ <main className='home-main' id= 'home'> 
+ 
+ <section className = 'home-one'>
         <h1>FULL-STACK,</h1>
         <h1>DEVELOPER</h1> 
         <p>G'DAY, I am just a dude trying to be a solid <b>Web Developer</b>, and a decent human being along the way.</p>
@@ -16,7 +18,28 @@ const Home = () => {
     <section className='home-two'>
             <img src= "/bg/IMG-20230813-WA0008-removebg-preview(1).png" alt= "me" />
     </section>
-    </main>
+ 
+ 
+ 
+ </main>
+    {/*<main className='home-main' id= 'home'>
+    <section className = 'home-one'>
+        <h1>FULL-STACK,</h1>
+        <h1>DEVELOPER</h1> 
+        <p>G'DAY, I am just a dude trying to be a solid <b>Web Developer</b>, and a decent human being along the way.</p>
+        <Button className='btn btn-dark btn-sm'>Download Resume</Button>  
+    </section>
+    <section className='home-two'>
+            <img src= "/bg/IMG-20230813-WA0008-removebg-preview(1).png" alt= "me" />
+            <div>
+              
+              <img src="/bg/Mask group1.jpg"/>
+              <img src="/bg/v1043-042a-12.jpg"/>
+             
+            </div>
+            
+    </section>
+  </main> */}  
     <main className='home-main-two'>
       <h5><b>TECH STACK</b></h5>
     
@@ -40,11 +63,10 @@ const Home = () => {
       <section className='sect-one'>
         <h1>About Me</h1>
 
-        <p>Hi, I am Aditya Sharma, a passionate self-taught Full-Stack developer from India. While pursuing my Master's degree in Computational Mechanics, I quickly established a strong affinity for coding. 
-          The realms of Web Development and Blockchain Technology intrigued me to the core. 
+        <p>Hi, I am Aditya Sharma, a passionate self-taught Full-Stack developer from India. While pursuing my Master's degree in Computational Mechanics, I quickly established a strong affinity for coding.  
           As I dive deep into learning, exploring, and the building applications for real-world use, 
-          I find myself fully hooked in the realm of coding.
-          My goal is to evolve into a skilled web developer as I progress on this journey. .</p>
+          I find myself fully hooked in the realm of Web Development and Blockchain techonolgy.
+          My goal is to evolve into a skilled web developer as I progress on this journey.</p>
       </section>
 
       <section className='sect-two'>
@@ -56,6 +78,8 @@ const Home = () => {
         </p>
       </section>
     </main>
+
+    <main className='lol'></main>
 
 
     
