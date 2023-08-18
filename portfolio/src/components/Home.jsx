79c27f0713aee697188785217fe78ee33a64ec95
bruh.ngl0 <adityashrm500@gmail.com>
2023-08-18 13:47:49 +0530

    final

--- portfolio/src/components/Home.jsx
+++ portfolio/src/components/Home.jsx
@@ -74,19 +74,19 @@ const Home = () => {
 
         <div>
         <h3>Description</h3>
-        <p>Successfully developed ANIPRO e-commerce website, that features user-friendly authentication with JWT tokens for secure access. Implemented protected routes and user/admin functionalities, bolstered by seamless PayPal and debit/credit card payment integration. Leveraged RTK Query for automated hooks, streamlining API requests, resulting in an efficient and reduced codebase by over 1000 lines.</p> 
+        <p>Successfully developed PROSHOP e-commerce website, that features user-friendly authentication with JWT tokens for secure access. Implemented protected routes and user/admin functionalities, bolstered by seamless PayPal and debit/credit card payment integration. Leveraged RTK Query for automated hooks, streamlining API requests, resulting in an efficient and reduced codebase by over 1000 lines.</p> 
         </div>
      
         <Card style={{ width: '20rem' }}>
 
-          <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+          <Card.Img variant="top" src="/projects/projOne.png" />
           <Card.Body>
-            <Card.Title>Card Title</Card.Title>
+            <Card.Title>ProShop</Card.Title>
             </Card.Body>
       
           <Card.Body>
-            <Card.Link href="#">Card Link</Card.Link>
-            <Card.Link href="#">Another Link</Card.Link>
+            <Card.Link href="https://proshop-mkh2.onrender.com/" target= "_blank" >Live Demo</Card.Link>
+            <Card.Link href="https://github.com/bruhngl0/E-comm100hr" target = "_blank">GitHub</Card.Link>
           </Card.Body>
 
       
@@ -101,21 +101,21 @@ const Home = () => {
         
      
         <Card style={{ width: '20rem' }}>
-          <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+          <Card.Img variant="top" src="/projects/projTwo.png" />
           <Card.Body>
-            <Card.Title>Card Title</Card.Title>
+            <Card.Title>Xcrypto</Card.Title>
           </Card.Body>
      
           <Card.Body>
-            <Card.Link href="#">Card Link</Card.Link>
-            <Card.Link href="#">Another Link</Card.Link>
+            <Card.Link href="https://64df17d8fdca5d17f1f6f98f--delightful-douhua-b5a4d2.netlify.app/"  target= "_blank">Live Demo</Card.Link>
+            <Card.Link href="https://github.com/bruhngl0/try_threeReact" target= "_blank" >GitHub</Card.Link>
           </Card.Body>
         </Card>
   
 
         <div>
           <h3>Description</h3>
-          <p>Successfully developed ANIPRO e-commerce website, featuring user-friendly authentication with JWT tokens for secure access. Implemented protected routes and user/admin functionalities, bolstered by seamless PayPal and debit/credit card payment integration. Leveraged RTK Query and automated hooks, streamlining API requests and enhancing code organization, resulting in a cleaner and more efficient implementation. Reduced codebase by over 1000 lines.</p>  
+          <p>Dive into the captivating universe of digital assets with X Crypto, a meticulously crafted React application designed to empower you with real-time insights into the top 100 cryptocurrency exchanges and currencies. Built with a touch of innovation and a dash of elegance, X Crypto leverages the cutting-edge Chakra UI library to deliver a sleek, modern, and user-friendly interface that adapts flawlessly to any device.</p>  
         </div>
         
       </section>
@@ -124,18 +124,20 @@ const Home = () => {
 
         <div> 
           <h3>Description</h3>
-          <p>Successfully developed ANIPRO e-commerce website, featuring user-friendly authentication with JWT tokens for secure access. Implemented protected routes and user/admin functionalities, bolstered by seamless PayPal and debit/credit card payment integration. Leveraged RTK Query and automated hooks, streamlining API requests and enhancing code organization, resulting in a cleaner and more efficient implementation. Reduced codebase by over 1000 lines.</p>  
+          <p>Introducing "VideoHUB": Your Ultimate Destination for Seamless Learning Through Comprehensive Video Lectures and Courses!
+
+Embark on a transformative learning journey with "VideoHUB," a dynamic React JSX and JavaScript-powered platform meticulously crafted to revolutionize how you absorb educational content. Designed to cater to both learners and educators, Video HUB provides an immersive haven where full-fledged video lectures and comprehensive courses come to life, all in one seamless and interruption-free environment.</p>  
         </div>
      
         <Card style={{ width: '20rem' }}>
-            <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+            <Card.Img variant="top" src="/projects/projThree.png" style ={{height: '20vh'}}/>
             <Card.Body>
-                <Card.Title>Card Title</Card.Title> 
+                <Card.Title>VideoHub</Card.Title> 
             </Card.Body>
      
             <Card.Body>
-                <Card.Link href="#">Card Link</Card.Link>
-                <Card.Link href="#">Another Link</Card.Link>
+                <Card.Link href="https://aquamarine-churros-d4fd59.netlify.app/" target= "_blank">Live Demo</Card.Link>
+                <Card.Link href="https://github.com/bruhngl0/try_twoReact" target= "_blank">GitHub</Card.Link>
             </Card.Body>
         </Card>
   
@@ -148,20 +150,20 @@ const Home = () => {
        
      
         <Card style={{ width: '20rem' }}>
-          <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+          <Card.Img variant="top" src="/projects/projFour.png"  style ={{height: '20vh'}}/>
         <Card.Body>
-            <Card.Title>Card Title</Card.Title>
+            <Card.Title>Techy Star</Card.Title>
         </Card.Body>
      
         <Card.Body>
-            <Card.Link href="#">Card Link</Card.Link>
-            <Card.Link href="#">Another Link</Card.Link>
+            <Card.Link href="https://64df16c3fdca5d17a2f6f7d9--moonlit-sorbet-e9289a.netlify.app/" target= "_blank">Live Demo</Card.Link>
+            <Card.Link href="https://github.com/bruhngl0/try_oneReact" target= "_blank">GitHub</Card.Link>
         </Card.Body> 
       </Card>
         
       <div> 
         <h3>Description</h3>
-        <p>Successfully developed ANIPRO e-commerce website, featuring user-friendly authentication with JWT tokens for secure access. Implemented protected routes and user/admin functionalities, bolstered by seamless PayPal and debit/credit card payment integration. Leveraged RTK Query and automated hooks, streamlining API requests and enhancing code organization, resulting in a cleaner and more efficient implementation. Reduced codebase by over 1000 lines.</p> 
+        <p>Step into the realm of cutting-edge solutions and unparalleled consultancy with "TechyStar," a captivating one-page React application that stands as a beacon of excellence for B2B services. Seamlessly blending the power of React and the artistry of Sass, TechyStar presents a captivating digital platform designed to provide unmatched services and strategic guidance to businesses on their journey to success.</p> 
       </div>
     </section>  
     </main>
